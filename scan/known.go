82f9460ec9fc7c47a39ccc14843ab0ef445e4efe
Keshav <bihanikeshav@gmail.com>
2023-09-21 12:18:06 +0530

    modsagain
    meh

--- scan/known.go
+++ scan/known.go
@@ -3,7 +3,9 @@ package scan
 // data from https://www.iana.org/assignments/service-names-port-numbers/service-names-port-numbers.csv
 var knownPorts = map[int]string{
 	// 80: "http",
-	554: "rtsp",
-	// 8080: "http-alt",
+	554:  "rtsp",
+	8000: "def",
+	8080: "http-alt",
+	8443: "hikvision-enhanced-something",
 	8554: "rtsp-alt",
 }
